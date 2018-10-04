b7eecc40ee05e30f74c338a828e5a2365a51b00d
Chris Clark <chrisclark@Chriss-MacBook-Pro.local>
2018-10-04 15:06:53 +0800

    Complete ch 2 challenge 3

---
+++ 02-manual-movement/challenge3/ZombieConga/ZombieConga/GameScene.swift
@@ -0,0 +1,159 @@
+import SpriteKit
+class GameScene: SKScene {
+    // MARK: - Properties
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPoint.zero
+    let zombieRotateRadiansPerSec: CGFloat = 4.0 * π
+    
+    let playableRect: CGRect
+    
+    let zombieInitialPosition = CGPoint(x: 400, y: 400)
+    
+    var lastTouchLocation:CGPoint
+    
+    // MARK: - Lifecycle
+    
+    override init(size: CGSize) {
+        let maxAspectRatio:CGFloat = 2.16 // 1
+        let playableHeight = size.width / maxAspectRatio // 2
+        let playableMargin = (size.height-playableHeight)/2.0 // 3
+        playableRect = CGRect(x: 0,
+                              y: playableMargin,
+                              width: size.width,
+                              height: playableHeight) // 4
+        lastTouchLocation = zombieInitialPosition
+        super.init(size: size) // 5
+    }
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented") // 6
+    }
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed: "background1")
+        
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.zPosition = -1
+        addChild(background)
+        
+        zombie.position = zombieInitialPosition
+        addChild(zombie)
+        
+        //    // Gesture recognizer example
+        //    // Uncomment this and the handleTap method, and comment the touchesBegan/Moved methods to test
+        //    let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        //    view.addGestureRecognizer(tapRecognizer)
+        
+        debugDrawPlayableArea()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        print("\(dt*1000) milliseconds since last update")
+        
+        let amountToMove = velocity * CGFloat(dt)
+        if (zombie.position - lastTouchLocation).length() < amountToMove.length() {
+            zombie.position = lastTouchLocation
+            velocity = CGPoint.zero
+        } else if velocity != CGPoint.zero {
+            move(sprite: zombie, velocity: velocity)
+            rotate(sprite: zombie, direction: velocity, rotateRadiansPerSec: zombieRotateRadiansPerSec)
+        }
+        boundsCheckZombie()
+    }
+    
+    // MARK: - Helpers
+    
+    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = velocity * CGFloat(dt)
+        print("Amount to move: \(amountToMove)")
+        sprite.position += amountToMove
+    }
+    
+    func moveZombieToward(location: CGPoint) {
+        let offset = location - zombie.position
+        let direction = offset.normalized()
+        velocity = direction * zombieMovePointsPerSec
+    }
+    
+    func boundsCheckZombie() {
+        let bottomLeft = CGPoint(x: 0, y: playableRect.minY)
+        let topRight = CGPoint(x: size.width, y: playableRect.maxY)
+        if zombie.position.x <= bottomLeft.x {
+            zombie.position.x = bottomLeft.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.x >= topRight.x {
+            zombie.position.x = topRight.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.y <= bottomLeft.y {
+            zombie.position.y = bottomLeft.y
+            velocity.y = -velocity.y
+        }
+        if zombie.position.y >= topRight.y {
+            zombie.position.y = topRight.y
+            velocity.y = -velocity.y
+        }
+    }
+    
+    func sceneTouched(touchLocation:CGPoint) {
+        lastTouchLocation = touchLocation
+        moveZombieToward(location: touchLocation)
+    }
+    
+    func debugDrawPlayableArea() {
+        let shape = SKShapeNode(rect: playableRect)
+        shape.strokeColor = SKColor.red
+        shape.lineWidth = 4.0
+        addChild(shape)
+    }
+    
+    func rotate(sprite: SKSpriteNode, direction: CGPoint, rotateRadiansPerSec: CGFloat) {
+        let shortest = shortestAngleBetween(angle1: sprite.zRotation, angle2: direction.angle)
+        var amountToRotate = rotateRadiansPerSec * CGFloat(dt)
+        if abs(shortest) < amountToRotate {
+            amountToRotate = abs(shortest)
+        }
+        amountToRotate *= shortest.sign()
+        sprite.zRotation += amountToRotate
+    }
+    
+    //  @objc func handleTap(recognizer: UIGestureRecognizer) {
+    //    let viewLocation = recognizer.location(in: self.view)
+    //    let touchLocation = convertPoint(fromView: viewLocation)
+    //    sceneTouched(touchLocation: touchLocation)
+    //  }
+    
+    // MARK: - UIResponder
+    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation: touchLocation)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation: touchLocation)
+    }
+
+}

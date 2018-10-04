6fc84b084d9db0629931cc94f0ca0199a7dda4ce
Chris Clark <chrisclark@Chriss-MacBook-Pro.local>
2018-10-04 10:32:41 +0800

    Complete challenge1

---
+++ 02-manual-movement/challenge1/ZombieConga/ZombieConga/MyUtils.swift
@@ -0,0 +1,55 @@
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+    left = left - right
+}
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+func *= (left: inout CGPoint, right: CGPoint) {
+    left = left * right
+}
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+func *= (point: inout CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+func /= ( left: inout CGPoint, right: CGPoint) {
+    left = left / right
+}
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+func /= (point: inout CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+    
+    var angle: CGFloat {
+        return atan2(y, x)
+    }
+}

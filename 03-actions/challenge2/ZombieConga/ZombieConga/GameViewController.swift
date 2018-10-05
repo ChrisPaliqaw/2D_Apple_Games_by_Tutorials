6a88bb3770ac5666c35e9e904fe897bd4f4aa892
Chris Clark <chrisclark@Chriss-MacBook-Pro.local>
2018-10-05 09:46:51 +0800

    Add ch 3 challenge2, which is now a copy of final

---
+++ 03-actions/challenge2/ZombieConga/ZombieConga/GameViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import SpriteKit
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene =
+            GameScene(size:CGSize(width: 2048, height: 1536))
+        
+        let skView = self.view as! SKView
+        skView.showsFPS = false
+        skView.showsNodeCount = false
+        skView.ignoresSiblingOrder = true
+        
+        scene.scaleMode = .aspectFill
+        
+        skView.presentScene(scene)
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}

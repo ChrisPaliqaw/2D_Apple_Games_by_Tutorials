f085141ee819ad517ab14954e89d0053656c6b8a
Chris Clark <chrisclark@Chriss-MacBook-Pro.local>
2018-10-08 11:12:40 +0800

    Complete ch 4 challenge1

--- 04-scenes/challenge1/ZombieConga/ZombieConga/GameViewController.swift
+++ 04-scenes/challenge1/ZombieConga/ZombieConga/GameViewController.swift
@@ -4,7 +4,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let scene =
-            GameScene(size:CGSize(width: 2048, height: 1536))
+            MainMenuScene(size:CGSize(width: 2048, height: 1536))
         
         let skView = self.view as! SKView
         skView.showsFPS = false

00c93f786ea256e87f3c1d04ac7c98fc03035275
Chris Clark <chrisclark@Chriss-MacBook-Pro.local>
2018-10-10 21:04:22 +0800

    Add ch 9 final, currently a copy of starter

---
+++ 09-intermediate-physics/final/CatNap/CatNap/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  CatNap
+//
+//  Created by Chris Clark on 10/9/18.
+//  Copyright © 2018 Qalang. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        isPaused = true
+        isPaused = false
+    }
+    
+    
+}

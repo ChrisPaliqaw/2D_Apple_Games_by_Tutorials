fb49eeb495264d8a06d5244e3428e7a51460f8d6
Chris Clark <chris@qalang.com>
2020-06-22 14:52:03 +0800

    Replace ch 8 final project Remove old ch 8 challenges

--- 08-beginning-physics/challenge2/SpriteKitPhysicsTest.playground/Sources/SupportCode.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import CoreGraphics
-
-public func delay(seconds: TimeInterval,
-                  completion: @escaping () -> Void) {
-    DispatchQueue.main.asyncAfter(deadline: .now() + seconds,
-                                  execute: completion)
-}
